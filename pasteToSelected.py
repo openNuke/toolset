1417247fc11be61376686ad5393d1dfc8f759123
vfxwiki <rafal@movingimagearts.com>
2015-02-23 00:47:00 +0000

    Update pasteToSelected.py

--- pasteToSelected.py
+++ pasteToSelected.py
@@ -1,3 +1,8 @@
+#author: Frank Rueter
+#dateCreated: 19/02/2012
+#source: http://www.nukepedia.com/python/nodegraph/pasteToSelected/ v1.1
+#licence: https://github.com/vfxwiki/nukeArtistToolkit/blob/master/README.md
+
 import nuke
 
 def toggleSelection(node):
