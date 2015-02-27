bf6602e8c6600238ad8c5d589ad2fe4af60e5643
opennuke <rafal@0me.co.uk>
2015-02-27 16:17:41 +0000

    Update _load.py

--- _load.py
+++ _load.py
@@ -100,6 +100,7 @@ class UI_enumerationSelect(nukescripts.PythonPanel):
 class toolSetData():
 	def __init__(self):
 		global rootPath
+		self.rootPath=rootPath
 	def selectSet(self):
 		if self.licence():		  
 			#### Select Location of Release Path ####
@@ -107,8 +108,8 @@ class toolSetData():
 			if pLoc.showModalDialog():
 				selectedLocationLabel = pLoc.typeKnob.value()
 				if selectedLocationLabel=='local':
-					rootPath = os.path.split(nuke.getFilename('Select _load.json', '_load.json'))[0]
-			choicePath = os.path.join(rootPath, "_load.json")
+					self.rootPath = os.path.split(nuke.getFilename('Select _load.json', '_load.json'))[0]
+			choicePath = os.path.join(self.rootPath, "_load.json")
 			#### Get Tool Release Data ####
 			toolSetDict = toolSetData().getData(choicePath).gotData
 			#### Select witch release to load ####
@@ -128,7 +129,7 @@ class toolSetData():
 			print selectToolSetList
 			for tool in selectToolSetList:		
 				#### Load Tool Selection ####
-				toolsetPath = os.path.join(rootPath, tool+'.json')
+				toolsetPath = os.path.join(self.rootPath, tool+'.json')
 				self.toolDict = toolSetData().getData(toolsetPath).gotData
 				print '================--=====[]['
 				print self.toolDict
