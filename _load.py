f31282732ccc482510ab8e8067f3c33afd827ca4
opennuke <rafal@0me.co.uk>
2015-02-27 16:19:43 +0000

    Update _load.py

--- _load.py
+++ _load.py
@@ -111,7 +111,7 @@ class toolSetData():
 					self.rootPath = os.path.split(nuke.getFilename('Select _load.json', '_load.json'))[0]
 			choicePath = os.path.join(self.rootPath, "_load.json")
 			#### Get Tool Release Data ####
-			toolSetDict = toolSetData().getData(choicePath).gotData
+			toolSetDict = toolSetData().getData(choicePath)
 			#### Select witch release to load ####
 			releaseLabelChoices=[]
 			toolSeChoices=toolSetDict.keys()
