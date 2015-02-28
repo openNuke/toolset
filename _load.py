e19664a896de641a8efa6f5754e09fa99ccffb28
opennuke <rafal@0me.co.uk>
2015-02-28 18:53:43 +0000

    Update _load.py

--- _load.py
+++ _load.py
@@ -140,9 +140,9 @@ class toolSetData():
 			documentation = self.toolDict['documentation']
 			source = self.toolDict['source']
 			
-			self.toolsDict.update({toolType:{category:{}}})
-			self.toolsDict{toolType:{category:{'file':file}}}
-			
+			self.toolsDict.update({toolType:{category}})
+			self.toolsDict.file{toolType:{category}} = {'file':file}
+			#self.toolsDict{toolType:{'category'}}) = category
 			#self.toolsDict.update({toolType:{category:{'file':file}}})
 			
 			#self.toolsDict.update({toolType:{category:{'tooltip':tooltip}}})
