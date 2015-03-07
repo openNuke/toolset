c0880261d28dde17a21f4b69f626157f7978e231
opennuke <rafal@0me.co.uk>
2015-03-07 23:31:42 +0000

    Update _load.py

--- _load.py
+++ _load.py
@@ -90,8 +90,7 @@ class toolSetWidget(QtGui.QWidget):
                 nuke.nodePaste("%clipboard%")
         elif os.path.splitext(os.path.split(toolPath)[1])[1]==".py":
             print call
-            exec compile(self.toolData, '<string>', 'exec') in ns
-            print ns
+            exec self.toolData.call
             
 ########################                
     
