cc10fe90fa2aee1b6f215a683c507f3c5e1d0415
opennuke <rafal@0me.co.uk>
2015-03-16 01:16:07 +0000

    Update pasteToSelected.py

--- pasteToSelected.py
+++ pasteToSelected.py
@@ -4,23 +4,23 @@
 #licence:https://github.com/openNuke/toolkit/blob/master/LICENCE
 #version:1.1
 #Documentation:http://www.nukepedia.com/python/pasteToSelected
-
 import nuke
-
-def toggleSelection(node):
-    newValue = not node['selected'].value()
-    node['selected'].setValue(newValue)
+class pasteToSelected():
+    def __init__(self):
+        if not nuke.selectedNodes():
+            nuke.nodePaste('%clipboard%')
+          return
+       selection = nuke.selectedNodes()
+        for node in selection:
+            toggleSelection(node)
+        for node in selection:
+            node['selected'].setValue(1)
+            nuke.nodePaste('%clipboard%')
+            node['selected'].setValue(0)
+        for node in selection:
+            self.toggleSelection(node)
     
-def pasteToSelected ():
-    if not nuke.selectedNodes():
-        nuke.nodePaste('%clipboard%')
-        return
-    selection = nuke.selectedNodes()
-    for node in selection:
-        toggleSelection(node)
-    for node in selection:
-        node['selected'].setValue(1)
-        nuke.nodePaste('%clipboard%')
-        node['selected'].setValue(0)
-    for node in selection:
-        toggleSelection(node)
+    def toggleSelection(self.node):
+        newValue = not node['selected'].value()
+        node['selected'].setValue(newValue)
+    
