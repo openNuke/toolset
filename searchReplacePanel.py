8776c08d14ccc0e0359eb0bae189ea63f4fd0932
opennuke <rafal@0me.co.uk>
2015-03-16 00:10:31 +0000

    Update searchReplacePanel.py

--- searchReplacePanel.py
+++ searchReplacePanel.py
@@ -195,10 +195,8 @@ def addSRPanel():
         '''Run the panel script and add it as a tab into the pane it is called from'''
         myPanel = SearchReplacePanel.SearchReplacePanel()
         return myPanel.addToPane()
- 
-#THIS LINE WILL ADD THE NEW ENTRY TO THE PANE MENU
-nuke.menu('Pane').addCommand('SearchReplace', addSRPanel)
- 
-#THIS LINE WILL REGISTER THE PANEL SO IT CAN BE RESTORED WITH LAYOUTS
-nukescripts.registerPanel('com.ohufx.SearchReplace', addSRPanel)
+def loadSRPanel():
+        nuke.menu('Pane').addCommand('SearchReplace', addSRPanel)
+        nukescripts.registerPanel('com.ohufx.SearchReplace', addSRPanel) 
+
 #####################
